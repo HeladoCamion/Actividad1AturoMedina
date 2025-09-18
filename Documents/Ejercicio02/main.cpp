a7931a0486a17295980b1f3ce10a763e69122790
HeladoCamion <felipe.medina@edu.uah.es>
2025-09-18 23:44:17 +0200

    Workspace

---
+++ Documents/Ejercicio02/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	int x, y, z;
+	
+	cout << "Introduce el primer numero: ";
+	cin >> x;
+	cout << "Ahora el segundo numero: ";
+	cin >> y;	
+	z = x + y;
+	cout<<"La suma de" << x << "mas" << y << "es igual a: " << z << "." << endl;
+	
+	return 0;
+	
+	
+}
